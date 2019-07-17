477a4e46d41fa790a8c610a49e9770ffad58a03f
Javier Chames <haimch@gmail.com>
2019-07-17 18:18:36 +0300

    Removed uneeded lines

--- Server/Day4/Star2/server.py
+++ Server/Day4/Star2/server.py
@@ -20,29 +20,10 @@ def css(filepath):
 @get("/images/<filepath:re:.*\.(jpg|png|gif|ico|svg)>")
 def img(filepath):
     return static_file(filepath, root="./static/images")
-
-
-#  Dynamic routes:
-# @get("/")
-# @view("signup.html")
-# def index():
-    # return
     
 @route('/')
 def index():
     return static_file("singup.html", root='templates/') 
-
-
-# @route('/add', method="POST")
-# def add_user():
-#     fname=request.forms.get("usrname")
-#     lname= request.forms.get("lstname")
-#     # print(lname)
-#     template=Template('<h1>Welcome {{firstname}}!</h1>'+ 'We are happy to have you again {{firstname}} {{lastname}}</h1>')
-#     return template.render(firstname=fname)
-#     return template.render(lastname=lname)
-
-
 
 @route("/add" , method="POST")
 @view("welcome.html")
@@ -50,7 +31,6 @@ def welcome():
      fname=request.forms.get("usrname")
      lname= request.forms.get("lstname")
      return {"firstname": fname,"lastname": lname}
-
 
 if __name__ == "__main__":
     run(host="localhost", port=7000, debug=True, reloader=True)
